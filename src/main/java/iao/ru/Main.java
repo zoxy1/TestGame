22584458a3cffc34ac3a87ce4d9ed8a3eeaaee8d
zoxy1 <zoxy1@yandex.ru>
2017-08-25 20:13:07 +0700

    first commit

---
+++ src/main/java/iao/ru/Main.java
@@ -0,0 +1,11 @@
+package iao.ru;
+
+/**
+ * Created by Zoxy1 on 25.08.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        TestGame testGame = new TestGame();
+        testGame.run();
+    }
+}
---
+++ src/main/java/iao/ru/TestGame.java
@@ -0,0 +1,10 @@
+package iao.ru;
+
+/**
+ * Created by Zoxy1 on 25.08.17.
+ */
+public class TestGame {
+    public void run (){
+
+    }
+}
